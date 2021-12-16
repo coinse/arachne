changed to handle tasks other than mult-class classification (need to further fix for the patch part) & drop the name of the gradient tensor for grad_loss as this causes non-existing name error

arachne/auto_patch_vk.py
@@ -60,7 +60,7 @@ def patch(
 	assert num_data == len(data_y), "%d vs %d" % (num_data, len(data_y))
 	
 	from collections import Iterable
-	if not isinstance(data_y[0], Iterable):
+	if not isinstance(data_y[0], Iterable) and num_label > 2:
 		from utils.data_util import format_label
 		data_y = format_label(data_y, num_label)
 
@@ -88,17 +88,23 @@ def patch(
 
 	# compute prediction & corr_predictions
 	predictions = model.predict(data_X)
+	print ("raw predictions shape", predictions.shape)	
 	if len(predictions.shape) == 3:
 		predictions = predictions.reshape((predictions.shape[0], predictions.shape[-1]))
+	
+	if len(predictions.shape) >= 2 and predictions.shape[-1] > 1: # first check whether this task is multi-class classification
+		correct_predictions = np.argmax(predictions, axis = 1)
+		correct_predictions = correct_predictions == np.argmax(data_y, axis = 1)
+	else:
+		correct_predictions = np.round(predictions).flatten() == data_y
 
-	correct_predictions = np.argmax(predictions, axis = 1)
-	correct_predictions = correct_predictions == np.argmax(data_y, axis = 1)
 	print ("The predictions", correct_predictions.shape)
 	if not only_loc:
 		indices_to_target = data_util.split_into_wrong_and_correct(correct_predictions)
 		#check whether given predef_indices_to_chgd to wrong is actually correct
 		if predef_indices_to_chgd is not None:  # Since, here, we asssume an ideal model
 			diff = set(predef_indices_to_chgd) - set(indices_to_target['wrong'])
+			print (len(set(predef_indices_to_chgd)), len(set(indices_to_target['wrong'])))
 			assert len(diff) == 0, diff 
 		indices_to_target['wrong'] = predef_indices_to_chgd
 	else: # only loc, so do not need to care about correct and wrong classification

arachne/record_misclf_indices.py
@@ -25,7 +25,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-if args.which_data != 'GTSRB':
+if args.which_data in ['fashion_mnist', 'cifar10']: #!= 'GTSRB':
 	if args.which_data == 'cifar10':
 		if bool(args.is_train):
 			dataset = torchvision.datasets.CIFAR10(root=args.datadir, train=True,
@@ -56,19 +56,35 @@ if args.which_data != 'GTSRB':
 
 	X = np.asarray(X)
 	y = np.asarray(y)
-else: # gtsrb
-	train_data, test_data = data_util.load_data('GTSRB', args.datadir, with_hist = bool(args.w_hist))
-	
+elif args.which_data in ['GTSRB', 'imbd']: # gtsrb
+	train_data, test_data = data_util.load_data(args.which_data, args.datadir, with_hist = bool(args.w_hist))
 	if bool(args.is_train):
 		X,y = train_data
 	else:
 		X,y = test_data
-		
+
+else:# will be deleted later
+	rain_data, test_data = data_util.load_data(args.which_data, args.datadir)
+	if bool(args.is_train):
+		X,y = train_data
+	else:
+		X,y = test_data
+
+	from sklearn.preprocessing import MinMaxScaler
+	import pandas as pd
+	datafile = "data/lstm/airline-passengers.csv"
+	dataframe = pd.read_csv(datafile, usecols=[1], engine='python')
+	dataset = dataframe.values
+	dataset = dataset.astype('float32')
+
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	scaler.fit(dataset)	
+
 
 loaded_model = load_model(args.model)
 loaded_model.summary()
 
-if args.which_data in ['cifar10', 'GTSRB']: # and also GTSRB
+if args.which_data in ['cifar10', 'GTSRB', 'imdb', 'simple_lstm']: # and also GTSRB
 	predicteds = loaded_model.predict(X)
 else:
 	if is_input_2d:
@@ -76,15 +92,31 @@ else:
 	else:
 		predicteds = loaded_model.predict(X).reshape(-1, 10)
 
-pred_labels = np.argmax(predicteds, axis = 1)
+print ("predicted shape", predicteds.shape)
+if args.which_data != 'simple_lstm':
+	if predicteds.shape[-1] > 1:
+		pred_labels = np.argmax(predicteds, axis = 1)
+	else:
+		pred_labels = np.round(predicteds).flatten()
+else: # might be deleted later
+	pred_labels = scaler.inverse_transform(predicteds)	
+	y = scaler.inverse_transform(y.reshape(-1,1))
+	y = np.int32(np.round(y.reshape(-1,)))
+	pred_labels = np.int32(np.round(pred_labels.reshape(-1,)))
+
 os.makedirs(args.dest, exist_ok = True)
 
 init_preds = [['index', 'true', 'pred']]
 misclfs = [['index','true','pred']]
 cnt = 0
 for i, (true_label, pred_label) in enumerate(zip(y, pred_labels)):
-	if pred_label != true_label:
-		misclfs.append([i,true_label,pred_label])
+	if args.which_data != 'simple_lstm':
+		if pred_label != true_label:
+			misclfs.append([i,true_label,pred_label])
+	else:
+		if np.abs(pred_label - true_label) > 30:
+			misclfs.append([i,true_label,pred_label])
+
 	init_preds.append([i,true_label,pred_label])
 	if true_label == pred_label:
 		cnt += 1

arachne/run_localise.py
@@ -175,11 +175,8 @@ def compute_gradient_to_loss(path_to_keras_model, idx_to_target_layer, X, y,
 	#	assert model is not None
 	#else:
 	model = load_model(path_to_keras_model, compile = False)
-	target = model.layers[idx_to_target_layer].weights[0] 
-
-	num_label = int(model.output.shape[-1])
-	y_tensor = tf.placeholder(tf.float32, shape = [None, num_label], name = 'labels')
-
+	target = model.layers[idx_to_target_layer].weights[0]
+	y_tensor = tf.keras.Input(shape = list(model.output.shape)[1:], name = 'labels')
 	if loss_func == 'softmax':
 		# might be changed as the following two
 		loss_tensor = tf.nn.softmax_cross_entropy_with_logits_v2(
@@ -201,9 +198,7 @@ def compute_gradient_to_loss(path_to_keras_model, idx_to_target_layer, X, y,
 
 	tensor_grad = tf.gradients(
 		loss_tensor,
-		target, 
-		name = 'loss_grad')
-
+		target)#, 
 	# since this might cause OOM error, divide them 
 	num = X.shape[0]
 	if by_batch:
@@ -217,19 +212,13 @@ def compute_gradient_to_loss(path_to_keras_model, idx_to_target_layer, X, y,
 	
 	gradients = []
 	for chunk in chunks:
-		_gradient = K.get_session().run(tensor_grad, feed_dict={model.input: X[chunk], y_tensor: y[chunk]})[0]
+		_gradient = K.get_session().run(tensor_grad, feed_dict={model.input: X[chunk], y_tensor: y[chunk].reshape(-1,1)})[0]
 		gradients.append(_gradient)
-		#print ("tensor grad", tensor_grad)
-		#print ("\t", _gradient.shape)
 
-	#print (np.asarray(gradients).shape)
 	gradient = np.sum(np.asarray(gradients), axis = 0)
-	#print (gradient[:10])
-	#import sys; sys.exit()	
 	gradient = np.abs(gradient)
 	if not wo_reset:
 		reset_keras([gradient, loss_tensor, y_tensor])
-	import sys; sys.exit()
 	return gradient
 	
 
@@ -355,8 +344,14 @@ def sample_input_for_loc_sophis(
 	## in auto_patch.patch: new_indices_to_target = list(indices_to_correct) + list(indices_to_selected_wrong) 
 	sample the indices to changed and unchanged behaviour later used for localisation 
 	"""
-	pred_labels = np.argmax(predictions, axis = 1)
-	y_labels = np.argmax(ys, axis = 1)
+	if len(ys.shape) > 1 and ys.shape[-1] > 1:
+		pred_labels = np.argmax(predictions, axis = 1)
+		y_labels = np.argmax(ys, axis = 1) 
+	else:
+		pred_labels = np.round(predictions).flatten()
+		y_labels = ys
+
+	print ("ex",y_labels)
 	_indices = np.zeros(len(indices_to_unchgd) + len(indices_to_chgd))
 	_indices[:len(indices_to_unchgd)] = indices_to_unchgd
 	_indices[len(indices_to_unchgd):] = indices_to_chgd
@@ -366,6 +361,8 @@ def sample_input_for_loc_sophis(
 	indices_to_unchgd = np.asarray(indices_to_unchgd); indices_to_unchgd.sort()
 	_indices_to_chgd = np.where(pred_labels != y_labels)[0]; _indices_to_chgd.sort()
 	indices_to_chgd = np.asarray(indices_to_chgd); indices_to_chgd.sort()
+
+	print ("indics_to_unchgd", indices_to_unchgd[:10])
 	assert all(indices_to_unchgd == _indices[_indices_to_unchgd])
 	assert all(indices_to_chgd == np.sort(_indices[_indices_to_chgd]))
 	###  checking end  ###
@@ -384,7 +381,6 @@ def sample_input_for_loc_sophis(
 	num_total_sampled = 0
 	for uniq_label,vs in grouped_by_label.items():
 		num_sample = int(np.round(num_chgd * len(vs)/num_unchgd))
-		print ("++", num_sample, len(vs)/num_unchgd)
 		if num_sample <= 0:
 			num_sample = 1
 		
@@ -785,7 +781,7 @@ def compute_FI_and_GL(
 			# this should be fixed to process a list of weights (or we can call it twice), and accept other loss function
 			tensor_w_kernel, tensor_w_recurr_kernel = model.layers[idx_to_tl].weights[:2]
 			if loss_funcs is None:
-				loss_func = 'softmax' #'binary_crossentropy'
+				loss_func = 'binary_crossentropy'
 			else:
 				from collections.abc import Iterable 
 				loss_func = loss_funcs[idx_to_tl] if not isinstance(loss_funcs, str) and isinstance(loss_funcs, Iterable) else loss_funcs
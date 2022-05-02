e6b9a955efa730f859f6707ceb48cc55811d3284
jjsohn <kasio555@kaist.ac.kr>
2022-05-02 11:17:20 +0200

    rq6 script

---
+++ arachne/source/rq6.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+datadir=$1
+dest=$2
+pa=2
+seed=0 # 0 .. 29
+
+model_dir="../final_data/models/rq6"
+iter_num=100
+index_dir="../final_data/indices"
+logdir="logs/rq6"
+
+if [ ! -d "$logdir" ]; then
+    mkdir $logdir
+fi
+
+python3 main.py \
+-datadir $datadir/lfw_data\
+-which lfw_vgg \
+-which_data lfw \
+-patch_key rq6.0.$seed \
+-seed $seed \
+-path_to_keras_model $model_dir/LFW_gender_classifier_best.h5 \
+-iter_num 100 \
+-target_indices_file $index_dir/lfw/test/lfw.init_pred.indices.csv \
+-dest $dest \
+-patch_aggr $pa \
+-batch_size 512 \
+-female_lst_file $datadir/lfw_np/female_names_lfw.txt \
+-top_n 0 \
+-num_label 2 > $logdir/pa$pa.$seed.out &
+wait $!
